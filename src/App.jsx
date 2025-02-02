dd4c16a10b29e211e66107e45e36204a63fc18c7
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2025-02-02 00:28:19 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Welcome to My Personal Blog</h1>
+      </header>
+      <main className="content">
+        <p>This is where the blog posts will go.</p>
+      </main>
+      <footer className="footer">
+        <p>&copy; 2023 My Personal Blog</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
